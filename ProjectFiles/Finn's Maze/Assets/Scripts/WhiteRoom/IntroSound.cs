c1f2ab654253eeea37f919f497c15848d0809f51
Nathan S <n_stowe@u.pacific.edu>
2017-06-20 15:28:40 -0700

    Updated project for Unity 5.6.1, added intro sound

---
+++ ProjectFiles/Finn's Maze/Assets/Scripts/WhiteRoom/IntroSound.cs
@@ -0,0 +1,16 @@
+﻿//IntroSound by Nathan Stowe
+//This script plays the "Big Boom" sound at initialization
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioSource bigBoom = GetComponent<AudioSource>();
+		bigBoom.Play();
+	}
+
+}
